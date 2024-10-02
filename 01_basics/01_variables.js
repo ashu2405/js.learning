8304fb055464fb029fa7cb3b02262ef61f44ca1a
Ashutosh Shukla <83330273+ashu2405@users.noreply.github.com>
2024-10-02 06:48:47 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,36 @@
+const accountId = 2405
+let accountEmail = "ashutosh74598@gmail.com"
+let accountCity = "Lucknow"
+let accountState = "Public"
+let accountPassword = "7459897322"
+let accountPercent = 75
+
+
+console.log(accountId);
+
+/* 
+
+prefer not to use var
+because of issue in block space and functional space
+
+*/
+
+console.table([accountId, accountEmail, accountCity, accountState, accountPassword, accountPercent]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
